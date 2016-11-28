08bc401b4ac8388fb594bb0fcc5906930674dd56
mikertb <mike@rtb.cat>
2016-11-28 16:09:23 +0800

    Inventory & 3rd Party page pagination swap update

--- js/bluekai.js
+++ js/bluekai.js
@@ -69,7 +69,7 @@ function page_submit(e){
                 setTimeout(function(){
                     loader.close();
                     // Payload here
-                    $("#pagination_container").html(result.pagination);
+                    $("#pagination_container_bottom").html(result.pagination);
                     $("#table_container").html(result.table);
                     //-------------
                 },delay);
