fff45da4e60722159e6043371f9da814af199671
mikertb <mike@rtb.cat>
2016-12-14 10:59:11 +0800

    Menu update: 3rd party data.

--- temp/js/app.dmp.js
+++ temp/js/app.dmp.js
@@ -2,12 +2,69 @@
     var config = {
         url : "http://sandbox.lat.com.es/index.php/info/dmp",
         loading: false,
-        last_data: null
+        last_data: null,
+        brands: {
+            bk_branded_data : "All",
+            bk_branded_data_acxiom : "Acxiom",
+            bk_branded_data_adadvisorbyneustar : "AdAdvisor",
+            bk_branded_data_addthis : "AddThis",
+            bk_branded_data_affinityanswers : "Afinity Answers",
+            bk_branded_data_alc : "ALC",
+            bk_branded_data_alliant : "Alliant",
+            bk_branded_data_analyticsiq : "AnalyticsIQ",
+            bk_branded_data_areyouahuman : "Are You a Human",
+            bk_branded_data_beintoo : "Beintoo",
+            bk_branded_data_bluekangaroo : "Bluekangaroo",
+            bk_branded_data_bombora : "Bombora",
+            bk_branded_data_comscore : "Comscore",
+            bk_branded_data_connexity : "Connexity",
+            bk_branded_data_crosspixel : "Crosspixel",
+            bk_branded_data_datacratic : "Datacratic",
+            bk_branded_data_datalab : "Datalab",
+            bk_branded_data_dataline : "Dataline",
+            bk_branded_data_datalogix : "Datalogix",
+            bk_branded_data_datamentors : "Datamentors",
+            bk_branded_data_datamyx : "Datamyx",
+            bk_branded_data_dataxpand : "Dataxpand",
+            bk_branded_data_delidatax : "DelidataX",
+            bk_branded_data_dunbradstreet : "Dun & Bradstreet",
+            bk_branded_data_evite : "Evite",
+            bk_branded_data_experian : "Experian",
+            bk_branded_data_experianuk : "Experian UK",
+            bk_branded_data_financialaudiences : "Financial Audiences",
+            bk_branded_data_forbes : "Forbes",
+            bk_branded_data_gfkmri : "GFK",
+            bk_branded_data_ibehavior : "iBehavior",
+            bk_branded_data_infogroup : "infogroup",
+            bk_branded_data_iriproscorescpg : "IRi",
+            bk_branded_data_ixi : "IXI",
+            bk_branded_data_kantarmediatgi : "Kantar Media",
+            bk_branded_data_lotame : "Lotame",
+            bk_branded_data_mastercard : "Mastercard",
+            bk_branded_data_mediasource : "Media Source",
+            bk_branded_data_merkle : "Merkle",
+            bk_branded_data_omnidigitalbymeritdirect : "OmniDigital",
+            bk_branded_data_omnibus : "Omnibus",
+            bk_branded_data_profound : "Profound",
+            bk_branded_data_pushspring : "Pushsprin",
+            bk_branded_data_scanbuy : "Scanbuy",
+            bk_branded_data_sirdata : "Sirdata",
+            bk_branded_data_sms : "SMS",
+            bk_branded_data_solvemedia : "Solve Media",
+            bk_branded_data_transunion : "Transunion",
+            bk_branded_data_trusignal : "Trusignal",
+            bk_branded_data_twinedata : "Twinedata",
+            bk_branded_data_v12group : "V12 Group",
+            bk_branded_data_vendigi : "Vendigi",
+            bk_branded_data_visavueaudiencespoweredbydlx : "VisaVue",
+            bk_branded_data_webbula : "Webbula"
+        }
     }
     var data = {items:[],page:{current:1,total:0}};
     var obj = {
         $form : $('#search_form'),
         $brands : $('#search_form li[data-category]'),
+        $brands_scrollbox: $('#search_form ul'),
         $th : $('#table_container th[data-sort]'),
         $tbody : $('#table_container tbody'),
         $sort_box: $('.table-control select.sorting'),
@@ -26,9 +83,31 @@
         obj.$prev_btn.on('click',prevPage);
         obj.$next_btn.on('click',nextPage);
         obj.$th.on('click',sortColumn);
-        update();
+        $('.suppliers .item a').click(setBrand);
+        if(location.hash){
+            var hashed_brand = location.hash.replace('#','').split('=');
+            if(hashed_brand.length === 2){
+                if(hashed_brand[0] === "brand" && hashed_brand[1].length > 2){
+                    setBrand('bk_branded_data_'+hashed_brand[1]);
+                }
+            }
+            else{update();}
+        }
+        else{update();}
     }
     function render(){
+        // Update page title.
+        var brand_data = obj.$form.find('input[name="category"]').val();
+        var brand_nraw = "";
+        if(brand_data.length > 15){brand_nraw = brand_data.replace('bk_branded_data_',"");}
+        else{brand_nraw = brand_data.replace('bk_branded_data',"");}
+        if(brand_nraw != ""){location.hash = "brand="+brand_nraw;}
+        $('#page_title_brand').text(config.brands[brand_data]);
+        // Update selected brand.
+        obj.$brands.removeClass('active');
+        var brand_scroller = obj.$form.find('ul');
+        var brand_selected = obj.$form.find('[data-category="'+brand_data+'"]');
+        brand_selected.addClass('active');
         // Loading status.
         if(config.loading){
             $('#loader').css('display','block');
@@ -106,18 +185,51 @@
                         // Remove sidebar toggle.
                         $('#side_bar').removeClass('toggled');
                         render();
+                        obj.$brands_scrollbox.scrollTop(0);
+                        var brand_selected = obj.$form.find('li.active');
+                        var amount_scroll = brand_selected.offset().top - obj.$brands_scrollbox.offset().top;
+                        obj.$brands_scrollbox.animate({scrollTop:amount_scroll},300);
+                        console.log(amount_scroll);
                     },500);
                 }
             });
         }
     }
     function setBrand(e){
-        var entry = (e.target.nodeName == "LI")? $(e.target) : $(e.target).parents("li");
-        obj.$form.find('input[name="category"]').val(entry.attr('data-category'));
-        obj.$brands.removeClass('active');
-        obj.$form.find('input[name="page"]').val(1);
-        entry.addClass('active');
-        update();
+        if(e){
+            if(e.type === "click"){
+                var tagel = e.target.nodeName;
+                var entry = "";
+                if(tagel === "LI"){
+                    entry = $(e.target);
+                    obj.$form.find('input[name="category"]').val(entry.attr('data-category'));
+                    obj.$form.find('input[name="page"]').val(1);
+                }
+                else if(tagel === "IMG"){
+                    entry = $(e.target).parents("li");
+                    obj.$form.find('input[name="category"]').val(entry.attr('data-category'));
+                    obj.$form.find('input[name="page"]').val(1);
+                }
+                else if(tagel === "A"){
+                    entry = $(e.target);
+                    var brand = 'bk_branded_data_'+entry.attr('href').split('=')[1];
+                    obj.$form.find('input[name="category"]').val(brand);
+                    obj.$form.find('input[name="page"]').val(1);
+                    $('.megamenu li.active').trigger('click');
+                }
+                update();
+            }
+            else if(typeof e === "string"){
+                var brand = obj.$form.find('[data-category="'+e+'"]');
+                if(brand.length == 1){
+                    obj.$brands.removeClass('active');
+                    brand.addClass('active');
+                    obj.$form.find('input[name="category"]').val(e);
+                    update();
+                }
+                else{update();}
+            }
+        }
     }
     function sortBy(e){
         var sort_input = obj.$form.find('input[name="sort_by"]');
